f83b53b56d206ba8e4c6cf6ce8e744b79cef5f44
syntaxkim <lovems0219@gmail.com>
2018-10-08 22:55:15 +0900

    javascript to control comment form added

---
+++ static/location.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $("#comment").submit(function() {
+
+        if (!$("#comment input[name=comment]").val())
+        {
+            alert("No comment to submit.")
+            return false;
+        }
+        else if ($("#comment input[name=comment]").val().length > 100)
+        {
+            alert("Comment is too long. Please make it less than 100 characters.");
+            return false;
+        }
+        return true;
+    });
+});
