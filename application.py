5b1d95821a81c33134bb4af268b68758deee6bee
syntaxkim <lovems0219@gmail.com>
2018-10-10 02:16:28 +0900

    Markup class deleted

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, request, redirect, url_for, escape, json, Markup, flash
+from flask import Flask, session, render_template, request, redirect, url_for, escape, json, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
