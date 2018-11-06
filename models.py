5e69fdb4b56de4feb0d37a09fa8170a695daee40
syntaxkim <lovems0219@gmail.com>
2018-11-06 19:38:21 +0900

    constraint updated

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
 
 class Location(db.Model):
