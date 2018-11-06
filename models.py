53bff5ecbe70b6bc6cdcb31fa7a019aa7d9019e1
syntaxkim <lovems0219@gmail.com>
2018-11-07 00:23:24 +0900

    methods added to classes

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 """ This file is not part of the appliation but for the reference when using ORM """
 
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from passlib.hash import pbkdf2_sha256
 
 db = SQLAlchemy()
 
@@ -12,6 +14,14 @@ class User(db.Model):
     name = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
 
+    def add_user(self):
+        try:
+            user = User(name=self.name, password=pbkdf2_sha256.hash(self.password))
+            db.session.add(user)
+            db.session.commit()
+        except:
+            raise Exception
+
 class Location(db.Model):
     __tablename__ = "locations"
     id = db.Column(db.Integer, primary_key=True)
@@ -20,13 +30,37 @@ class Location(db.Model):
     state = db.Column(db.String, nullable=False)
     lat = db.Column(db.Float, nullable=False)
     long = db.Column(db.Float, nullable=False)
-    population = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=True)
+    checkins = db.relationship("Checkin", backref="location", lazy=True)
+
+    def add_location(self):
+        try:
+            location = Location(zipcode=self.zipcode, city=self.city, state=self.state, lat=self.lat, long=self.long, population=self.population)
+            db.session.add(location)
+            db.session.commit()
+        except:
+            raise Exception
+
+    def get_checkins(self):
+        try:
+            checkins = Checkin.query.filter_by(location_id=self.id).all()
+            return checkins
+        except:
+            raise Exception
 
 class Checkin(db.Model):
     __tablename__ = "checkins"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     comment = db.Column(db.String(100), nullable=False)
-    time = db.Column(db.DateTime, nullable=False)
+    time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     location_id = db.Column(db.Integer, db.ForeignKey("locations.id"), nullable=False)
+
+    def add_checkin(self):
+        try:
+            checkin = Checkin(name=self.name, comment=self.comment, location_id=self.location_id)
+            db.session.add(checkin)
+            db.session.commit()
+        except:
+            raise Exception
 
